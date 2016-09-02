43b32a6c5d6a2f30bac24b0ee5019511a642a290
gantian127 <jamy127@foxmail.com>
2016-09-02 16:55:00 -0600

    model run: enable resource download and package upload. Still need file validation for parameters and input data files

--- tethysapp/ueb_app/controllers.py
+++ tethysapp/ueb_app/controllers.py
@@ -1,4 +1,10 @@
 import json
+import tempfile
+import os
+import shutil
+import zipfile
+
+import xmltodict
 
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
@@ -7,11 +13,12 @@ from django.http import HttpResponse
 from tethys_sdk.gizmos import TextInput, SelectInput,DatePicker, GoogleMapView
 
 from hs_restclient import HydroShare, HydroShareAuthBasic
+from hydrogate import HydroDS
 
 from epsg_list import EPSG_List
 from model_input_utils import *
 from user_settings import *
-import xmltodict
+
 
 
 # home page views
@@ -283,10 +290,75 @@ def model_run_load_metadata(request):
 
 @login_required()
 def model_run_submit_execution(request):
-    ajax_response = {
-        'status': 'Success',
-        'result': 'job submitted'
-    }
+    try:
+        # authentication
+        auth = HydroShareAuthBasic(hs_name, hs_password)
+        hs = HydroShare(auth=auth)
+        client = HydroDS(username=hydrods_name, password=hydrods_password)
+
+        # download resource bag
+        res_id = request.POST['resource_list']
+        temp_dir = tempfile.mkdtemp()
+        hs.getResource(res_id, temp_dir, unzip=False)
+        bag_path = os.path.join(temp_dir, res_id+'.zip')
+        zf = zipfile.ZipFile(bag_path)
+        zf.extractall(temp_dir)
+        zf.close()
+        os.remove(bag_path)
+
+        # move all files in the same folder
+        model_input_folder = os.path.join(temp_dir, res_id,  'data', 'contents')
+        model_files_path_list = [os.path.join(model_input_folder, name) for name in os.listdir(model_input_folder)]
+
+        while model_files_path_list:
+            added_files_list = []
+
+            for model_file_path in model_files_path_list:
+
+                if os.path.isfile(model_file_path) and os.path.splitext(model_file_path)[1] == '.zip':
+                    zf = zipfile.ZipFile(model_file_path, 'r')
+                    zf.extractall(model_input_folder)
+                    extract_file_names = zf.namelist()
+                    added_files_list += [os.path.join(model_input_folder, name) for name in extract_file_names]
+                    zf.close()
+                    os.remove(model_file_path)
+
+                elif os.path.isdir(model_file_path):
+                    for dirpath, _, filenames in os.walk(model_file_path):
+                        for name in filenames:
+                            sub_file_path = os.path.abspath(os.path.join(dirpath, name))
+                            new_file_path = os.path.join(model_input_folder, name)
+                            shutil.move(sub_file_path, new_file_path)
+                            added_files_list.append(new_file_path)
+                    shutil.rmtree(model_file_path)
+
+            model_files_path_list = added_files_list
+
+
+        # TODO: model parameter and data files check
+
+        # upload the model files to HydroDS
+        model_files_path_list = [os.path.join(model_input_folder, name) for name in os.listdir(model_input_folder)]
+        zip_file_path = os.path.join(model_input_folder, 'input_package.zip')
+        zf = zipfile.ZipFile(zip_file_path, 'w')
+        for file_path in model_files_path_list:
+            zf.write(file_path)
+        zf.close()
+        upload_zip_file_url = client.upload_file(file_to_upload=zip_file_path)
+        client.delete_my_file(upload_zip_file_url.split('/')[-1])
+
+        ajax_response = {
+            'status': 'Success',
+            'result': upload_zip_file_url
+        }
+
+
+    except Exception as e:
+        ajax_response = {
+            'status': 'Error',
+            'result': 'Failed to submit the model execution.' + e.message
+        }
+
     return HttpResponse(json.dumps(ajax_response))
 
 
