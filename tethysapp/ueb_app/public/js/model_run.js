8bbbf53fb7a6951a025597c3a71832b3613c5cbe
gantian127 <jamy127@foxmail.com>
2016-09-02 11:28:38 -0600

    allow drawing of bounding box and outlet point on map

--- tethysapp/ueb_app/public/js/model_run.js
+++ tethysapp/ueb_app/public/js/model_run.js
@@ -5,16 +5,6 @@ var drawingManager;
 
 $(document).ready(function() {
 
-    // Draw marker on text change
-    $("#outlet_x").bind('input', drawMarkerOnTextChange);
-    $("#outlet_y").bind('input', drawMarkerOnTextChange);
-
-    // Draw rectangle on text change
-    $("#north_lat").bind('input', drawRectangleOnTextChange);
-    $("#south_lat").bind('input', drawRectangleOnTextChange);
-    $("#east_lon").bind('input', drawRectangleOnTextChange);
-    $("#west_lon").bind('input', drawRectangleOnTextChange);
-
     // Hide the metadata section and model run btn when user changes the selection
     $('#resource_list').change(function(){
         $('#submit-model-run-btn').hide();
@@ -62,6 +52,26 @@ $(document).ready(function() {
 
                         document.getElementById('load-metadata-response').style.display = 'block';
                         document.getElementById('submit-model-run-btn').style.display = 'block';
+
+                        // draw marker on map
+                        if (json_response.result.outlet_x != 'unknown' && json_response.result.outlet_y != 'unknown'){
+                            var myLatLng = {lat: parseFloat(json_response.result.outlet_y),
+                                            lng: parseFloat(json_response.result.outlet_x)};
+                            drawMarkerOnTextChange(myLatLng);
+                        }
+
+                        // draw rectangle on map
+                        if (json_response.result.north_lat != 'unknown' && json_response.result.south_lat != 'unknown' &&
+                        json_response.result.east_lon != 'unknown' && json_response.result.west_lon != 'unknown'){
+                            var bounds = {
+                                    north: parseFloat(json_response.result.north_lat),
+                                    south: parseFloat(json_response.result.south_lat ),
+                                    east: parseFloat(json_response.result.east_lon),
+                                    west: parseFloat(json_response.result.west_lon)
+                                };
+                            drawRectangleOnTextChange(bounds);
+                        }
+
                     }
                     else {
                         $('#load-metadata-error').text(json_response.result);
@@ -86,46 +96,6 @@ $(document).ready(function() {
         // run service
         }
 
-
-//        $('#submit-response').hide();
-//        $('#submit-model-input-btn').prop('disabled', true);
-//        $('#wait').modal('show');
-
-//        $.ajax({
-//            type: user_form.attr('method'),
-//            url: user_form.attr('action'),
-//            data: user_form.serialize(),
-//
-//            success: function(result) {
-//                console.log(result);
-//                json_response = JSON.parse(result);
-//                console.log(json_response);
-//                alert('happy');
-//                $('#submit-response').show()
-//                if (json_response.status == 'Error'){
-//                    document.getElementById("submit-response").style.backgroundColor = '#ffebe6';
-//                }
-//                else {
-//                    document.getElementById("submit-response").style.backgroundColor = '#eafaea';
-//                }
-//
-//                $('#response-status').text(json_response.status)
-//                $('#response-result').text(json_response.result);
-//            },
-//
-//            error: function() {
-//                alert('sad');
-//                $('#response-status').text('Error')
-//                $('#response-result').text('Failed to run the web service. Please try it again.');
-//            },
-//
-//            complete: function(){
-//                alert('complete');
-//                $('#wait').modal('hide');
-//                $('#submit-model-input-btn').prop('disabled', false);
-//            }
-//        });
-//        return false;
     });
 
 });
@@ -138,79 +108,11 @@ map = new google.maps.Map(mapDiv, {
 });
 
 map.setMapTypeId('terrain');
-var drawingManager = new google.maps.drawing.DrawingManager({
-    drawingControl: true,
-    drawingControlOptions: {
-      position: google.maps.ControlPosition.TOP_CENTER,
-      drawingModes: [
-        google.maps.drawing.OverlayType.MARKER,
-        google.maps.drawing.OverlayType.RECTANGLE
-      ]
-     },
-    rectangleOptions: {
-        editable: true,
-        draggable: true
-    }
- });
-
- drawingManager.setMap(map);
-
- google.maps.event.addListener(drawingManager, 'rectanglecomplete', function (rectangle) {
-
-    for (var i = 0; i < allRectangles.length; i++) {
-            allRectangles[i].setMap(null);
-     };
-    allRectangles.push(rectangle);
-
-    var coordinates = (rectangle.getBounds());
-    processDrawing(coordinates, 'rectangle');
-    rectangle.addListener('bounds_changed', function () {
-        var coordinates = (rectangle.getBounds());
-        processDrawing(coordinates, "rectangle");
-    });
-
- });
-
- google.maps.event.addListener(drawingManager, 'markercomplete', function (marker) {
-
-    for (var i = 0; i < allMarkers.length; i++) {
-            allMarkers[i].setMap(null);
-     };
-
-    var coordinates = (marker.getPosition());
-    processDrawing(coordinates, 'marker');
-    allMarkers.push(marker);
- });
 
 } // end of initmap function
 
 
-
-function processDrawing (coordinates, shape) {
-    if (shape == "rectangle"){
-        // get coordinate value
-        var bounds = {
-            north: parseFloat(coordinates.getNorthEast().lat()),
-            south: parseFloat(coordinates.getSouthWest().lat()),
-            east: parseFloat(coordinates.getNorthEast().lng()),
-            west: parseFloat(coordinates.getSouthWest().lng())
-        };
-
-        // update form fields
-        $("#north_lat").val(bounds.north.toFixed(4));
-        $("#south_lat").val(bounds.south.toFixed(4));
-        $("#east_lon").val(bounds.east.toFixed(4));
-        $("#west_lon").val(bounds.west.toFixed(4));
-    }
-    else {
-        $("#outlet_x").val(parseFloat(coordinates.lng()).toFixed(4));
-        $("#outlet_y").val(parseFloat(coordinates.lat()).toFixed(4));
-    }
-} // end of processingDrawing function
-
-
-function drawMarkerOnTextChange(){
-    var myLatLng = {lat: parseFloat($("#outlet_y").val()), lng: parseFloat($("#outlet_x").val())};
+function drawMarkerOnTextChange(myLatLng){
 
     // Delete previous drawings
     for (var i = 0; i < allMarkers.length; i++) {
@@ -221,13 +123,11 @@ function drawMarkerOnTextChange(){
     var badInput = false;
 
     if (myLatLng.lat > 90 || myLatLng.lat < -90) {
-        alert('Latitude should between -90 and 90 degree');
         $("#outlet_y").val('');
         badInput = true;
     }
 
     if (myLatLng.lng > 180 || myLatLng.lng < -180) {
-        alert('Longitude should between -180 and 180 degree');
         $("#outlet_x").val('');
         badInput = true;
     }
@@ -248,14 +148,7 @@ function drawMarkerOnTextChange(){
 } // end of drawMarkerOnTextChange
 
 
-function drawRectangleOnTextChange(){
-    var bounds = {
-        north: parseFloat($("#north_lat").val()),
-        south: parseFloat($("#south_lat").val()),
-        east: parseFloat($("#east_lon").val()),
-        west: parseFloat($("#west_lon").val())
-    };
-
+function drawRectangleOnTextChange(bounds){
     // Delete previous drawings
     for (var i = 0; i < allRectangles.length; i++) {
             allRectangles[i].setMap(null);
@@ -263,33 +156,25 @@ function drawRectangleOnTextChange(){
 
     // Bounds validation
     var badInput = false;
+
     // North
     if (bounds.north > 90 || bounds.north < -90) {
-        alert('Latitude should between -90 and 90 degree');
         badInput = true;
-        $("#north_lat").val('');
     }
 
     // East
     if (bounds.east > 180 || bounds.east < -180) {
-        alert('Longitude should between -180 and 180 degree');
         badInput = true;
-        $("#east_lon").val('');
     }
 
     // South
     if (bounds.south < -90 || bounds.south > 90) {
-        alert('Latitude should between -90 and 90 degree');
         badInput = true;
-        $("#south_lat").val('');
     }
 
     // West
     if (bounds.west < -180 || bounds.west > 180) {
-        alert('Longitude should between -180 and 180 degree');
         badInput = true;
-        $("#west_lon").val('');
-
     }
 
     if (badInput || isNaN(bounds.north) || isNaN(bounds.south) || isNaN(bounds.east) || isNaN(bounds.west)) {
@@ -299,15 +184,10 @@ function drawRectangleOnTextChange(){
     // Define the rectangle and set its editable property to true.
     var rectangle = new google.maps.Rectangle({
         bounds: bounds,
-        editable: true,
-        draggable: true
+        editable: false,
+        draggable: false,
     });
     rectangle.setMap(map);
-
-    rectangle.addListener('bounds_changed', function () {
-        var coordinates = (rectangle.getBounds());
-        processDrawing(coordinates, "rectangle");
-    });
 
     var southWest = new google.maps.LatLng(bounds.south,bounds.west);
     var northEast = new google.maps.LatLng(bounds.north,bounds.east);
