5edad87a698be03d3654c89dcdb019e9ed67d271
gantian127 <jamy127@foxmail.com>
2016-09-03 11:18:22 -0600

    model run: move the submit model run job function from controller.py to model_run_utils.py

--- tethysapp/ueb_app/controllers.py
+++ tethysapp/ueb_app/controllers.py
@@ -1,21 +1,16 @@
 import json
-import tempfile
-import os
-import shutil
-import zipfile
 
 import xmltodict
 
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-
 from tethys_sdk.gizmos import TextInput, SelectInput,DatePicker, GoogleMapView
 
 from hs_restclient import HydroShare, HydroShareAuthBasic
-from hydrogate import HydroDS
 
 from epsg_list import EPSG_List
+from model_run_utils import *
 from model_input_utils import *
 from user_settings import *
 
@@ -292,85 +287,13 @@ def model_run_load_metadata(request):
 
 @login_required()
 def model_run_submit_execution(request):
-    try:
-        # authentication
-        auth = HydroShareAuthBasic(hs_name, hs_password)
-        hs = HydroShare(auth=auth)
-        client = HydroDS(username=hydrods_name, password=hydrods_password)
-
-        # download resource bag
+    if request.is_ajax and request.method == 'POST':
         res_id = request.POST['resource_list']
-        temp_dir = tempfile.mkdtemp()
-        hs.getResource(res_id, temp_dir, unzip=False)
-        bag_path = os.path.join(temp_dir, res_id+'.zip')
-        zf = zipfile.ZipFile(bag_path)
-        zf.extractall(temp_dir)
-        zf.close()
-        os.remove(bag_path)
-
-        # move all files in the same folder
-        model_input_folder = os.path.join(temp_dir, res_id,  'data', 'contents')
-
-        if os.path.isdir(model_input_folder):
-            model_files_path_list = [os.path.join(model_input_folder, name) for name in os.listdir(model_input_folder)]
-
-            # unzip .zip and move folder files to the same temp_dir level
-            while model_files_path_list:
-                added_files_list = []
-
-                for model_file_path in model_files_path_list:
-
-                    if os.path.isfile(model_file_path) and os.path.splitext(model_file_path)[1] == '.zip':
-                        zf = zipfile.ZipFile(model_file_path, 'r')
-                        zf.extractall(model_input_folder)
-                        extract_file_names = zf.namelist()
-                        added_files_list += [os.path.join(model_input_folder, name) for name in extract_file_names]
-                        zf.close()
-                        os.remove(model_file_path)
-
-                    elif os.path.isdir(model_file_path):
-                        for dirpath, _, filenames in os.walk(model_file_path):
-                            for name in filenames:
-                                sub_file_path = os.path.abspath(os.path.join(dirpath, name))
-                                new_file_path = os.path.join(model_input_folder, name)
-                                shutil.move(sub_file_path, new_file_path)
-                                added_files_list.append(new_file_path)
-                        shutil.rmtree(model_file_path)
-
-                model_files_path_list = added_files_list
-
-
-            # TODO: model parameter and data files check
-
-            # upload the model input and parameter files to HydroDS
-            model_files_path_list = [os.path.join(model_input_folder, name) for name in os.listdir(model_input_folder)]
-            zip_file_path = os.path.join(model_input_folder, 'input_package.zip')
-            zf = zipfile.ZipFile(zip_file_path, 'w')
-            for file_path in model_files_path_list:
-                zf.write(file_path)
-            zf.close()
-            upload_zip_file_url = client.upload_file(file_to_upload=zip_file_path)
-            client.delete_my_file(upload_zip_file_url.split('/')[-1]) #TODO clean this line for testing
-
-            # TODO: call model run service
-
-            ajax_response = {
-                'status': 'Success',
-                'result': upload_zip_file_url
-            }
-        else:
-            ajax_response = {
-                'status': 'Error',
-                'result': 'The model instance resource includes no model input data and parameter files.'
-            }
-
-        # remove the tempdir
-        shutil.rmtree(temp_dir)
-
-    except Exception as e:
+        ajax_response = submit_model_run_job(res_id, hs_name, hs_password,hydrods_name, hydrods_password)
+    else:
         ajax_response = {
             'status': 'Error',
-            'result': 'Failed to submit the model execution.' + e.message
+            'result': 'Please verify that the request is ajax call with post method'
         }
 
     return HttpResponse(json.dumps(ajax_response))
--- tethysapp/ueb_app/model_run_utils.py
+++ tethysapp/ueb_app/model_run_utils.py
@@ -1,3 +1,97 @@
-def get_model_input_res_metadata(res_id):
-    res_metadata = {}
-    return res_metadata
+"""
+utility functions for model run service
+"""
+
+import shutil
+import os
+import zipfile
+import tempfile
+
+from hs_restclient import HydroShare, HydroShareAuthBasic
+from hydrogate import HydroDS
+
+
+def submit_model_run_job(res_id, hs_name, hs_password, hydrods_name, hydrods_password):
+    try:
+        # authentication
+        auth = HydroShareAuthBasic(hs_name, hs_password)
+        hs = HydroShare(auth=auth)
+        client = HydroDS(username=hydrods_name, password=hydrods_password)
+
+        # download resource bag
+        temp_dir = tempfile.mkdtemp()
+        hs.getResource(res_id, temp_dir, unzip=False)
+        bag_path = os.path.join(temp_dir, res_id + '.zip')
+        zf = zipfile.ZipFile(bag_path)
+        zf.extractall(temp_dir)
+        zf.close()
+        os.remove(bag_path)
+
+        # move all files in the same folder
+        model_input_folder = os.path.join(temp_dir, res_id, 'data', 'contents')
+
+        if os.path.isdir(model_input_folder):
+            model_files_path_list = [os.path.join(model_input_folder, name) for name in os.listdir(model_input_folder)]
+
+            # unzip .zip and move folder files to the same temp_dir level
+            while model_files_path_list:
+                added_files_list = []
+
+                for model_file_path in model_files_path_list:
+
+                    if os.path.isfile(model_file_path) and os.path.splitext(model_file_path)[1] == '.zip':
+                        zf = zipfile.ZipFile(model_file_path, 'r')
+                        zf.extractall(model_input_folder)
+                        extract_file_names = zf.namelist()
+                        added_files_list += [os.path.join(model_input_folder, name) for name in extract_file_names]
+                        zf.close()
+                        os.remove(model_file_path)
+
+                    elif os.path.isdir(model_file_path):
+                        for dirpath, _, filenames in os.walk(model_file_path):
+                            for name in filenames:
+                                sub_file_path = os.path.abspath(os.path.join(dirpath, name))
+                                new_file_path = os.path.join(model_input_folder, name)
+                                shutil.move(sub_file_path, new_file_path)
+                                added_files_list.append(new_file_path)
+                        shutil.rmtree(model_file_path)
+
+                model_files_path_list = added_files_list
+
+            # TODO: model parameter and data files check
+
+            # upload the model input and parameter files to HydroDS
+            model_files_path_list = [os.path.join(model_input_folder, name) for name in os.listdir(model_input_folder)]
+            zip_file_path = os.path.join(model_input_folder, 'input_package.zip')
+            zf = zipfile.ZipFile(zip_file_path, 'w')
+            for file_path in model_files_path_list:
+                zf.write(file_path)
+            zf.close()
+            upload_zip_file_url = client.upload_file(file_to_upload=zip_file_path)
+            client.delete_my_file(upload_zip_file_url.split('/')[-1])  # TODO clean this line for testing
+
+            # TODO: call model run service
+
+            model_run_job = {
+                'status': 'Success',
+                'result': upload_zip_file_url
+            }
+        else:
+            model_run_job = {
+                'status': 'Error',
+                'result': 'The model instance resource includes no model input data and parameter files.'
+            }
+
+        # remove the tempdir
+        shutil.rmtree(temp_dir)
+
+    except Exception as e:
+        if os.path.isdir(temp_dir):
+            shutil.rmtree(temp_dir)
+
+        model_run_job = {
+            'status': 'Error',
+            'result': 'Failed to submit the model execution.' + e.message
+        }
+
+    return model_run_job
